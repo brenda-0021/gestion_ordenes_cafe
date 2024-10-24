1c332f3564567497f03491d867360d7d24188292
Brenda Z <brenda.zamarripa5773@alumnos.udg.mx>
2024-10-24 13:03:12 -0600

    paleta de colores y logo navegador

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cafe: {
+          claro: "#F2D6A2", // Tono claro
+          medio: "#BF9663", // Tono medio
+          suave: "#F2DEC4", // Tono suave
+          oscuro: "#592411", // Tono oscuro
+          intenso: "#260E09", // Tono más oscuro
+        },
+      },
+    },
   },
   plugins: [],
 };
